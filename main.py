e6a10c41152a7b795d384cdc230ee3421433d565
Hanyi-Yuan <hanyi.yuan@students.asdrp.org>
2026-02-22 17:45:35 -0800

    Still needs polish, added a few checks for logic

--- main.py
+++ main.py
@@ -1,47 +1,70 @@
 from random import choice
 
-def word_check(letter=''): #function that returns a list of all coincidences of the letter
+def word_check(letter='',target=''):#function because I'm lazy
     result=[]
-    for i in range(len(target_word)):
-        if letter in target_word[i]:
+    for i in range(len(target)):
+        if letter in target[i]:
             result.append(i)
 
     return result
 
+def input_check(temp):#temp=input
+    flag=True
+    if len(temp)>1: #check lenght
+        return False
+    try:
+        int(temp) #check number
+        flag=False
+    except:
+        pass
+       
+    return flag
+
 print("Let's play hangman")
 
-word_access=open('word_bank.txt','r') #access previous file
-temp_ls=word_access.read() #reading file
-word_access.close() #closing file
+def hangman():
+    word_access=open('word_bank.txt','r') #access previous file
+    temp_ls=word_access.read() #reading file
+    temp_ls=['hello','testing']#temporary testing
+    word_access.close() #closing file
 
-target_word=choice(temp_ls) #getting random word
-wrong_guesses=[]
+    target_word=choice(temp_ls) #getting random word
+    wrong_guesses=[]
 
-print(target_word) #debug
+    print(target_word) #debug
 
-win=False
-display=['_' for i in target_word]#for the _____ print
-lives=2*len(target_word)-2 
-while lives>0:
-    print(str(display)) #str for easy read
-    user_input=input(f"Guess a letter(lives={lives}):\n")
-    check=word_check(user_input)#prompts for input, then checks the input
+    win=False
+    display=['_' for i in target_word]#for the _____ print
+    lives=2*len(target_word)-2 
+    while lives>0:
+        print(str(display)) #str for easy read
+        user_input=input(f"Guess a letter(lives={lives}):\n")
+        if(not input_check(user_input)):
+            print('Invalid input! Only single length letters are allowed!')
+            continue
+        check=word_check(user_input,target_word)#prompts for input, then checks the input
+        
+        try:#error handling and me being lazy
+            for j in check:
+                display[j]=user_input
+            check.pop(0)# triggers error if empty
+            print("Correct guess!")
+        except:
+            print("Wrong!")
 
-    try:#error handling and me being lazy
-        for j in check:
-            display[j]=user_input
-        check.pop(0)# triggers error if empty
-        print("Correct guess!")
-    except:
-        print("Wrong!")
+        win=not '_' in display
 
-    win=not '_' in display #when its filled
+        if win:
+            print(f'You won with {lives} lives remaining!')
+            break
+        else:
+            print(f"wrong guesses:{wrong_guesses}")
+        lives-=1
 
-    if win: #checking win conditions
-        print(f'You won with {lives} lives remaining!')
-        break
-    else:
-        print(f"wrong guesses:{wrong_guesses}")
-    lives-=1
+    print("you lost, try again next time."*(not win))
 
-print("you lost, try again next time."*(not win))#only prints when win=False
+while True:
+    hangman()
+    if(input('do you want to play again?(y/n)\n')=='y'):
+        hangman()
+    break
